12aa26fd1f58f633add6c5ff1eeb842c6344119c
Stewart Moreland <stewartmoreland@users.noreply.github.com>
2022-05-13 03:08:25 -0500

    fix subscriptable query for quotes

--- c3po/modules/slack.py
+++ c3po/modules/slack.py
@@ -324,7 +324,8 @@ class SlackEventHandler(object):
             elif 'quote' in request['event']['text']:
                 app.logger.info('Quotes requested')
                 order_by = func.random() if app.config['db_dialect'] == 'sqlite' or app.config['db_dialect'] == 'postgresql' else func.rand()
-                random_quote = StarWarsQuotes.query.order_by(order_by).first()
+                quotes = StarWarsQuotes.query
+                random_quote = quotes.order_by(order_by).first()
                 message['text'] = f"> {random_quote['quote']} - {random_quote['character']}"
 
             else:
